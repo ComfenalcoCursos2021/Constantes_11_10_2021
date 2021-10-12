16bc8f982ab5e57c660f746bc52adf853aac9272
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-10-11 21:32:08 -0500

    Ejercicio de variables y constantes 11-10-2021

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+    header("Content-Type: application/json");
+
+
+    //Declarar dos constantes de lo que quieran, una variables de lo que quieras
+
+
+
+
+
+    
+
+
+
+    define("PLANETA", (string) "Planeta natal donde viven los humanos ('Terricolas')");
+    var_dump(PLANETA);
+
+    define('MASA',  (string) 'La masa de del planeta tierra es: 9,807 m/s²');
+    var_dump(MASA);
+    
+    echo "\n";
+    
+    $vaso = (string) null;
+    var_dump($vaso);
+    $vaso = (string) "Agua";
+    var_dump($vaso);
+    $vaso = (string) "Cafe";
+    var_dump($vaso);
+    
+    
+    
+
+
+
+
+
+
+    // Contantes
+    // define("NOMBRES", (string) "MIGUEL ANGEL");
+    // echo NOMBRES;
+    
+
+
+
+
+    // Variables
+    // $NOMBRES_APELLIDOS = (string) null;
+
+    // var_dump($NOMBRES_APELLIDOS);
+
+    // $NOMBRES_APELLIDOS = (string) "Miguel";
+
+    // var_dump($NOMBRES_APELLIDOS);
+
+    // $NOMBRES_APELLIDOS = (string) "Andrea";
+
+    // var_dump($NOMBRES_APELLIDOS);
+
+?>
